ab4d47aa6a57ad4e33c7f73fc86906fb42c3df95
Raymond D. Powell <rayzer42@gmail.com>
2018-03-21 17:29:41 -0400

    first commit

---
+++ did-oauth2-server/server.js
@@ -0,0 +1,49 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    oauthserver = require('node-oauth2-server');
+ 
+var app = express();
+ 
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+app.use(bodyParser.json());
+
+const model = {
+  // We support returning promises.
+  getAccessToken: function() {
+    return new Promise('works!');
+  },
+
+  // Or, calling a Node-style callback.
+  getAuthorizationCode: function(done) {
+    done(null, 'works!');
+  },
+
+  // Or, using generators.
+  getClient: function*() {
+    yield somethingAsync();
+    return 'works!';
+  },
+
+  // Or, async/wait (using Babel).
+  getUser: async function() {
+    await somethingAsync();
+    return 'works!';
+  }
+};
+
+app.oauth = oauthserver({
+  model, // See below for specification 
+  grants: ['password'],
+  debug: true
+});
+ 
+app.all('/oauth/token', app.oauth.grant());
+ 
+app.get('/', app.oauth.authorise(), function (req, res) {
+  res.send('Secret area');
+});
+ 
+app.use(app.oauth.errorHandler());
+ 
+app.listen(3000);
